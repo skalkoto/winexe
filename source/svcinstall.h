1a3abde0be60c65afadbfa17f3db191eb1702c6c
Thomas Hood <jdthood@gmail.com>
2011-09-01 15:13:00 +0200

    Add new header files

---
+++ source/svcinstall.h
@@ -0,0 +1,25 @@
+/*
+   Copyright (C) Andrzej Hajda 2009
+   Contact: andrzej.hajda@wp.pl
+   License: GNU General Public License version 3
+*/
+
+#define SVC_INTERACTIVE 1
+#define SVC_IGNORE_INTERACTIVE 2
+#define SVC_INTERACTIVE_MASK 3
+#define SVC_FORCE_UPLOAD 4
+#define SVC_OS64BIT 8
+#define SVC_OSCHOOSE 16
+#define SVC_UNINSTALL 32
+#define SVC_SYSTEM 64
+
+NTSTATUS svc_install(struct tevent_context *ev_ctx, 
+                     const char *hostname,
+		     const char *service_name, const char *service_filename,
+		     unsigned char *svc32_exe, unsigned int svc32_exe_len,
+		     unsigned char *svc64_exe, unsigned int svc64_exe_len,
+		     struct cli_credentials * credentials, int flags);
+NTSTATUS svc_uninstall(struct tevent_context *ev_ctx,
+		       const char *hostname,
+		       const char *service_name, const char *service_filename,
+		       struct cli_credentials * credentials);
