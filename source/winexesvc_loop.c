1c77417fdac0ecf0e16bffad24446fb341b81114
Andrzej Hajda <andrzej.hajda@wp.pl>
2013-01-07 21:57:52 +0100

    added codepage reporting to winexesvc
    Signed-off-by: Andrzej Hajda <andrzej.hajda@wp.pl>

--- source/winexesvc_loop.c
+++ source/winexesvc_loop.c
@@ -196,6 +196,7 @@ finish:
 static int cmd_get(connection_context *c)
 {
 	static const char* var_version = "version";
+	static const char* var_codepage = "codepage";
 	char *cmdline;
 	int res = 0;
 
@@ -208,9 +209,12 @@ static int cmd_get(connection_context *c)
 	if ((strstr(cmdline, var_version) == cmdline) &&
             (cmdline[l = strlen(var_version)] == 0)) {
 		hprintf(c->pipe, "version 0x%04X\n", VERSION);
+	} else if ((strstr(cmdline, var_codepage) == cmdline) &&
+	            (cmdline[l = strlen(var_codepage)] == 0)) {
+		hprintf(c->pipe, "codepage 0x%04X\n", GetOEMCP());
 	} else {
 		hprintf(c->pipe, "error Unknown argument (%s)\n", c->cmd);
-	        goto finish;
+	    goto finish;
 	}
 	res = 1;
 finish:
